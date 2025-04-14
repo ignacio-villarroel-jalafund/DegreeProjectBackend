b0cb1cbf5efdcc0b151a599c45aac8adeb35528b
Ignacio Villarroel <ignaciovillarroel50@gmail.com>
2025-04-14 15:26:43 -0400

    feat: add user model

---
+++ src/app/models/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, String, Boolean
+from sqlalchemy.orm import relationship
+from app.core.database import Base
+from app.models.base import BaseModel
+
+class User(Base, BaseModel):
+    __tablename__ = "users"
+
+    email = Column(String, unique=True, index=True, nullable=False)
+    password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True)
+
+    ratings = relationship("Rating", back_populates="user")
+    favorites = relationship("Favorite", back_populates="user")
