24831caa1b93dee332bf512b9b4111d53bc50a89
Ignacio Villarroel <ignaciovillarroel50@gmail.com>
2025-04-24 21:13:12 -0400

    feat: add recipe search schema

--- src/app/schemas/recipe.py
+++ src/app/schemas/recipe.py
@@ -1,7 +1,11 @@
-from pydantic import BaseModel
-from typing import Optional
+from pydantic import BaseModel, HttpUrl
+from typing import Any, Dict, List, Optional
 from datetime import datetime
 import uuid
+
+class RecipeSearchResult(BaseModel):
+    title: str
+    url: HttpUrl
 
 class RecipeBase(BaseModel):
     recipe_name: str
@@ -9,36 +13,32 @@ class RecipeBase(BaseModel):
     cook_time: Optional[int] = None
     total_time: Optional[int] = None
     servings: Optional[int] = None
-    yield_amount: Optional[str] = None
     ingredients: str
     directions: str
     rating: Optional[float] = None
-    url: Optional[str] = None
+    url: HttpUrl = None
     cuisine_path: Optional[str] = None
     nutrition: Optional[str] = None
-    timing: Optional[str] = None
-    img_src: Optional[str] = None
+    img_src: Optional[HttpUrl] = None
 
 class RecipeCreate(RecipeBase):
+    url: HttpUrl
+    recipe_name: str
     pass
 
-class RecipeUpdate(BaseModel):
+class RecipeUpdate(RecipeBase):
     recipe_name: Optional[str] = None
-    prep_time: Optional[int] = None
-    cook_time: Optional[int] = None
-    total_time: Optional[int] = None
-    servings: Optional[int] = None
-    yield_amount: Optional[str] = None
-    ingredients: Optional[str] = None
-    directions: Optional[str] = None
-    rating: Optional[float] = None
-    url: Optional[str] = None
-    cuisine_path: Optional[str] = None
-    nutrition: Optional[str] = None
-    timing: Optional[str] = None
-    img_src: Optional[str] = None
+    url: Optional[HttpUrl] = None
 
-class RecipeInDBBase(RecipeBase):   
+class ScrapeRequest(BaseModel):
+    url: HttpUrl
+
+class ScrapedRecipeData(BaseModel):
+    recipe_name: Optional[str] = None
+    ingredients: Optional[List[str]] = None
+    directions: Optional[List[str]] = None
+
+class RecipeRead(RecipeBase):   
     id: uuid.UUID
     created_at: datetime
     updated_at: Optional[datetime] = None
@@ -46,8 +46,3 @@ class RecipeInDBBase(RecipeBase):
     class Config:
         from_attributes = True
 
-class RecipeRead(RecipeInDBBase):
-    pass
-
-class RecipeInDB(RecipeInDBBase):
-    pass
