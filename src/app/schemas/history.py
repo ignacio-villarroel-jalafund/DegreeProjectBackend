a64e32931f6d7453470c2f4b6a70c006b34848e3
Ignacio Villarroel <ignaciovillarroel50@gmail.com>
2025-06-12 18:58:52 -0400

    feat: add history schema

---
+++ src/app/schemas/history.py
@@ -0,0 +1,24 @@
+import uuid
+from datetime import datetime
+from typing import Optional, Dict, Any
+from pydantic import BaseModel
+
+from app.schemas.recipe import ScrapedRecipeData
+
+class HistoryBase(BaseModel):
+    recipe_data: Dict[str, Any]
+    source_url: Optional[str] = None
+    is_adapted: bool = False
+
+class HistoryCreate(HistoryBase):
+    user_id: uuid.UUID
+
+class HistoryRead(BaseModel):
+    id: uuid.UUID
+    created_at: datetime
+    recipe_data: ScrapedRecipeData
+    source_url: Optional[str] = None
+    is_adapted: bool
+
+    class Config:
+        from_attributes = True
