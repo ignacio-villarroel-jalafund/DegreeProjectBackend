e73a1dbed21e67298f50d4e3b8366fc978381eb5
Ignacio Villarroel <ignaciovillarroel50@gmail.com>
2025-07-22 18:22:10 -0400

    refactor: add diet and allergies models

--- src/app/models/__init__.py
+++ src/app/models/__init__.py
@@ -3,3 +3,5 @@ from app.models.favorite import Favorite
 from app.models.recipe import Recipe
 from app.models.user import User
 from app.models.history import History
+from app.models.diet import Diet
+from app.models.allergy import Allergy
