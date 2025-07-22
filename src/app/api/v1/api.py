c204b782a146c4a016bcb7907d3efdca45cee729
Ignacio Villarroel <ignaciovillarroel50@gmail.com>
2025-07-22 18:21:37 -0400

    refactor: add diets and diets routes

--- src/app/api/v1/api.py
+++ src/app/api/v1/api.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import locations, supermarkets, tasks, users, recipes
+from app.api.v1.endpoints import locations, supermarkets, tasks, users, recipes, diets, allergies
 
 api_router = APIRouter()
 
@@ -8,3 +8,5 @@ api_router.include_router(recipes.router, prefix="/recipes", tags=["recipes"])
 api_router.include_router(tasks.router, prefix="/tasks", tags=["tasks status"])
 api_router.include_router(locations.router, prefix="/locations", tags=["locations"])
 api_router.include_router(supermarkets.router, prefix="/supermarkets", tags=["supermarkets"])
+api_router.include_router(diets.router, prefix="/diets", tags=["diets"])
+api_router.include_router(allergies.router, prefix="/allergies", tags=["allergies"])
